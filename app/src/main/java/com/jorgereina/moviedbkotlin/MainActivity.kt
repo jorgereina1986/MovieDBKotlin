52880408b71ad1deab30cf27016091d2d320a1cd
Jorge Reina <j.reina@partner.samsung.com>
2019-03-07 18:07:29 -0500

    initial setup

--- app/src/main/java/com/jorgereina/moviedbkotlin/MainActivity.kt
+++ app/src/main/java/com/jorgereina/moviedbkotlin/MainActivity.kt
@@ -2,11 +2,16 @@ package com.jorgereina.moviedbkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, BuildConfig.TMDB_API_KEY)
     }
 }

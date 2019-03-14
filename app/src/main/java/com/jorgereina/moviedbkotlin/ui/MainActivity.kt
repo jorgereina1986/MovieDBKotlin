eff54c08cf0fb0209457b61f03cb178a34b9f8d2
Jorge Reina <j.reina@partner.samsung.com>
2019-03-14 17:00:19 -0400

    search working with viewmodel

--- app/src/main/java/com/jorgereina/moviedbkotlin/ui/MainActivity.kt
+++ app/src/main/java/com/jorgereina/moviedbkotlin/ui/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.jorgereina.moviedbkotlin.ui
 
 import android.app.SearchManager
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -10,15 +12,10 @@ import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.Menu
 import android.widget.SearchView
-import com.jorgereina.moviedbkotlin.BuildConfig
 import com.jorgereina.moviedbkotlin.R
 import com.jorgereina.moviedbkotlin.data.Movie
-import com.jorgereina.moviedbkotlin.service.ApiFactory
+import com.jorgereina.moviedbkotlin.viewmodel.MovieViewModel
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import retrofit2.HttpException
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,11 +24,13 @@ class MainActivity : AppCompatActivity() {
     private var movies = ArrayList<Movie>()
     private lateinit var adapter: MovieAdapter
     private lateinit var layoutManager: RecyclerView.LayoutManager
+    private lateinit var viewModel: MovieViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        viewModel = ViewModelProviders.of(this).get(MovieViewModel::class.java)
         layoutManager = LinearLayoutManager(this)
         adapter = MovieAdapter(movies)
         movies_rv.layoutManager = layoutManager
@@ -39,21 +38,6 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun loadMovies(query: String) {
-        val movieService = ApiFactory.tmdbApi
-        CoroutineScope(Dispatchers.Main).launch {
-            val popularMovieRequest = movieService.getMoviesAsync(BuildConfig.TMDB_API_KEY, query)
-            try {
-                val response = popularMovieRequest.await()
-                movies.addAll(response.results as ArrayList<Movie>)
-                adapter.notifyDataSetChanged()
-            } catch (e: Exception) {
-                Log.d(TAG, e.message)
-            } catch (e: HttpException) {
-                Log.d(TAG, e.message)
-            }
-        }
-    }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.options_menu, menu)
@@ -65,7 +49,12 @@ class MainActivity : AppCompatActivity() {
 
         if (Intent.ACTION_SEARCH == intent.action) {
             intent.getStringExtra(SearchManager.QUERY)?.also { query ->
-                loadMovies(query)
+                viewModel.getMovieSearched(query)
+                viewModel.getMovies().observe(this, Observer {
+                    movieList -> movies.addAll(movieList!!)
+                    adapter.notifyDataSetChanged()
+                    Log.d(TAG, movies[3].title)
+                })
             }
         }
 
---
+++ app/src/main/java/com/jorgereina/moviedbkotlin/viewmodel/MovieViewModelJava.java
@@ -0,0 +1,8 @@
+package com.jorgereina.moviedbkotlin.viewmodel;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MovieViewModelJava extends ViewModel {
+
+
+}
